b7ea94063ccb11e336645527bc20bb38af1218ab
Shubhankar <Shubhankar>
2016-09-29 09:07:15 +0530

    added while loop example

---
+++ java_tutorial/src/com/java/tut/looping/WhileLoopExample.java
@@ -0,0 +1,13 @@
+package com.java.tut.looping;
+
+public class WhileLoopExample {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while (i <= 10) {
+			System.out.println(i + "");
+			i++;
+		}
+	}
+
+}
